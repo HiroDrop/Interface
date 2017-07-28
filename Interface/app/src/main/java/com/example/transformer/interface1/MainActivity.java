327ad5565fba2407e53dcbabae47a71e40b52cd4
h-simada <h-simada@ist.osaka-u.ac.jp>
2017-07-29 01:50:20 +0900

    金さんの編集を反映

--- Interface/app/src/main/java/com/example/transformer/interface1/MainActivity.java
+++ Interface/app/src/main/java/com/example/transformer/interface1/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.transformer.interface1;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,17 +26,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         Button minusbtn = (Button)findViewById(R.id.minusbutton);
+
+        final Spinner action = (Spinner)findViewById(R.id.app1);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+
         minusbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                startApp();
+                String item = (String)action.getSelectedItem();
+
+                if(item.equals("カメラ")){
+                    startCamera();
+                }
+                else if(item.equals("ブラウザ")){
+                    startBrowser();
+                }
+                else if(item.equals("ダイアル")){
+                    startDial();
+                }
+                else{
+                    builder.setMessage("アプリを選んでください.");
+                    builder.show();
+                }
             }
         });
     }
 
-    private void startApp(){
+    private void startCamera(){
         Uri uri = Uri.parse("https://www.google.co.jp/");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    private void startDial(){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        startActivity(intent);
+    }
+    private void startBrowser(){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         startActivity(intent);
     }
 
